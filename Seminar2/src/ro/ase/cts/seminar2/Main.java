8e9cd393590a6ec6cd81c10c55a1f7460dce8f5e
Alina Dragne <dragnealina18@stud.ase.ro>
2021-03-04 09:21:32 +0200

    seminar2 final

--- Seminar2/src/ro/ase/cts/seminar2/Main.java
+++ Seminar2/src/ro/ase/cts/seminar2/Main.java
@@ -1,11 +1,25 @@
 package ro.ase.cts.seminar2;
 
+import ro.ase.cts.seminar2.exceptii.IllegalTransferException;
+import ro.ase.cts.seminar2.exceptii.InsufficientFundsException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		CurrentAccount c=new CurrentAccount(300, "IBAN123");
+		CurrentAccount c=new CurrentAccount(300, "IBAN1");
+		CurrentAccount account2=new CurrentAccount(200,"IBAN2");
+		System.out.println("suma disponibila dupa depozitare este:"+ c.getBalance());
+		System.out.println("creditul maxim pentru contul curent este:" + CurrentAccount.MAX_CREDIT);
+		c.deposit(200);
 		System.out.println("suma disponibila este:"+ c.getBalance());
-		//System.out.println("Creditul maxim pentru contul curent este:" + CurrentAccount.MAX_CREDIT);
+		try {
+			c.withdraw(200);
+			c.transfer(100, account2);
+		} catch (InsufficientFundsException | IllegalTransferException e) {
+			System.err.println(e.getMessage());
+		} 
+		System.out.println("suma disponibila in contul 1 este:"+ c.getBalance());
+		System.out.println("suma disponibila in contul 2 este:"+ account2.getBalance());
 	}
 
 }
