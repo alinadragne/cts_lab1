073ac0171ff6370c9f36a46a5d0b86fc0e45ad1a
Alina Dragne <dragnealina18@stud.ase.ro>
2021-04-01 08:21:48 +0300

    seminar 6 break

---
+++ Seminar6/src/ro/ase/cts/seminar6/builder/OfficeProduct.java
@@ -0,0 +1,17 @@
+package ro.ase.cts.seminar6.builder;
+
+public class OfficeProduct implements Product{
+	String productName;
+	
+	
+	public OfficeProduct(String productName) {
+		super();
+		this.productName = productName;
+	}
+	
+	@Override
+	public String getDescription() {
+		return "This is an office product";
+	}
+
+}
