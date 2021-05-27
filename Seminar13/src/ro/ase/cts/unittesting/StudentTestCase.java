e328e1dc9fb7d4f50111aae7e17fbc03d9af4dc2
Alina Dragne <dragnealina18@stud.ase.ro>
2021-05-27 08:28:20 +0300

    seminar 13 break

--- Seminar13/src/ro/ase/cts/unittesting/StudentTestCase.java
+++ Seminar13/src/ro/ase/cts/unittesting/StudentTestCase.java
@@ -1,4 +1,4 @@
-package ro.acs.seminar12.unittesting;
+package ro.ase.cts.unittesting;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,8 +8,16 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ro.ase.cts.seminar13.Student;
+import ro.ase.cts.seminar13.exceptions.StudentExceptionWrongValue;
+
 
 class StudentTestCase {
+	
+	Student student;
+	String defaultNume = "Anonim";
+	int defaultVarsta = 18;
+	int defaultNote[] = {}; 
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -23,17 +31,18 @@ class StudentTestCase {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		System.out.println("setUp was called");
+		student = new Student(defaultNume,defaultVarsta,defaultNote);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 		System.out.println("tearDown was called");
 	}
-
+	
+    /* right/conformance test */
 	@Test
 	void testStudentConstructorRight() {
-		String studentName = "Alex";
+		String studentName = "Alina";
 		int studentAge = 27;
 		int grades[] = {8, 8 ,9};
 		
@@ -44,7 +53,26 @@ class StudentTestCase {
 	}
 	
 	@Test
-	void test2() {
-		fail("Not yet implemented - test 2");
+	void testStudentConstructoriRightNotNull() {
+		int grades[] = {10, 10 ,9};
+		
+		Student student = null;
+		try{
+			student = new Student("Alina", 21, grades);
+	
+		} catch(Exception e) {
+			fail("Constructorul nu trb sa arunce exceptie");
+		}
+		assertNotNull(student);
 	}
+	
+	@Test(expected = StudentExceptionWrongValue.class)
+	void testStudentSetVarstaErrorCondition() {
+		
+		student.setVarsta(-1);	
+			
+	}
+	
+	
+	
 }
